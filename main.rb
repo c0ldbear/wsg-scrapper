8fecc6c4881e94cf0a5c96636194460210c7fc80
Teddy Juhlin-Henricson <teddy.juhlinhenricson@daresay.co>
2023-11-01 11:38:27 +0100

    Parse all tags and clean arrays

--- main.rb
+++ main.rb
@@ -4,23 +4,45 @@ require 'nokogiri'
 url = "https://w3c.github.io/sustyweb"
 response = HTTParty.get(url)
 
+stuff = {}
+
 if response.code == 200
     puts "Success!"
-    html_doc = Nokogiri::HTML(response.body)
+    html_doc = Nokogiri::HTML(response.body) #{ |conf| conf.noblanks }
     
     titles = []
-    html_doc.css('h3').each do | paragraph |
-        titles.append(paragraph.text)
+    html_doc.css('h3').each do | title |
+        titles.append(title.text)
     end
 
-    puts titles
+    print "titles array length: "
+    puts titles.length
 
-    html_doc.css('h4').each do | paragraph |
-        if paragraph.text.downcase.eql? "impact & effort"
-            puts paragraph.text
+    ratings = []
+    html_doc.css('dl.rating').each do | dl_tag |
+        # dl_tag.children.each do | children |
+        #     puts children
+        # end
+        dl_tag.css('dd').each do | rating |
+            ratings.append(rating.text)
         end
+
     end
 
+    print "ratings array length: "
+    puts ratings.length
+    puts ""
+
+    titles.shift(6) # 6 is the number of the first 5 topics in the Introduction
+    titles.pop(4)
+
+    puts "titles array new length: #{titles.length}"
+    puts titles[0..10]
+    puts ""
+
+    ratings.shift(9) # 9 is the number of the first 8 explanations of the Impact and Effort part
+    puts "ratings array new length: #{ratings.length}"
+    puts ratings[0..10]
 else 
     puts "Failed: #{response.code}"
 end
